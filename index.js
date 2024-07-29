9a5813fc08348abd639a36a1deee4d61d4752273
Subhrajit Chandra <2414101844@mujonline.edu.in>
2024-07-29 21:15:40 +0530

    edit died cluster

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ if (cluster.isPrimary) {
 
     cluster.on('exit', (worker, code, signal) => {
         console.log(`worker ${worker.process.pid} died`);
-        cluster.fork();
+        
     });
 } else {
     const app = express();
